0f7366480822f83003e2c9ce2630c7dc7891cc36
Hoshea Jiang <fgksgf@gmail.com>
2021-03-08 11:30:22 +0800

    调试视频摘要模块 (#13)
    * 合并RUN命令，简化Dockerfile
    
    * 添加模型下载命令
    
    * 删除多余的依赖
    
    * 封装视频摘要api

--- video_analysis/key_frame_extractor.py
+++ video_analysis/key_frame_extractor.py
@@ -7,6 +7,7 @@ import os
 import sys
 from scipy.signal import argrelextrema
 
+
 # 基于帧间差分的关键帧提取算法：
 # 我们知道，将两帧图像进行差分，得到图像的平均像素强度可以用来衡量两帧图像的变化大小。因此，基于帧间差分的平均强度，每当视频中的某一帧与前一帧画面内容产生了大的变化，我们便认为它是关键帧，并将其提取出来。
 # 这里一般有三种算法，基于差分强度的顺序，基于差分强度阈值，基于局部最大值。比较推荐使用第三种方法来提取视频的关键帧，我们选择具有平均帧间差分强度局部最大值的帧作为视频的关键帧，这种方法的提取结果在丰富度上表现更好一些，提取结果均匀分散在视频中。
@@ -16,43 +17,45 @@ from scipy.signal import argrelextrema
 def smooth(x, window_len=13, window='hanning'):
     s = np.r_[2 * x[0] - x[window_len:1:-1],
               x, 2 * x[-1] - x[-1:-window_len:-1]]
- 
+
     if window == 'flat':  # moving average
         w = np.ones(window_len, 'd')
     else:
         w = getattr(np, window)(window_len)
     y = np.convolve(w / w.sum(), s, mode='same')
     return y[window_len - 1:-window_len + 1]
- 
+
 
 class Frame:
     """class to hold information about each frame
     
     """
+
     def __init__(self, id, diff):
         self.id = id
         self.diff = diff
- 
+
     def __lt__(self, other):
         if self.id == other.id:
             return self.id < other.id
         return self.id < other.id
- 
+
     def __gt__(self, other):
         return other.__lt__(self)
- 
+
     def __eq__(self, other):
         return self.id == other.id and self.id == other.id
- 
+
     def __ne__(self, other):
         return not self.__eq__(other)
- 
- 
-def rel_change(a, b):
-   x = (b - a) / max(a, b)
-   return x
 
-def extract_keyframes(video_path: str) -> [str]:
+
+def rel_change(a, b):
+    x = (b - a) / max(a, b)
+    return x
+
+
+def extract_keyframes(video_path: str, save_dir: str) -> [str]:
     # 视频文件名
     video_name = video_path.split('/')[-1].split('.')[0]
 
@@ -67,9 +70,6 @@ def extract_keyframes(video_path: str) -> [str]:
     # Number of top sorted frames
     NUM_TOP_FRAMES = 50
 
-    # 存关键帧的路径
-    dir = './keyframes/' + video_name + '/'
-    os.makedirs('./keyframes/' + video_name)
     # smoothing window size
     len_window = int(50)
 
@@ -106,7 +106,7 @@ def extract_keyframes(video_path: str) -> [str]:
             keyframe_id_set.add(keyframe.id)
     if USE_THRESH:
         for i in range(1, len(frames)):
-            if (rel_change(np.float(frames[i - 1].diff), np.float(frames[i].diff)) >= THRESH):
+            if rel_change(np.float(frames[i - 1].diff), np.float(frames[i].diff)) >= THRESH:
                 keyframe_id_set.add(frames[i].id)
     if USE_LOCAL_MAXIMA:
         diff_array = np.array(frame_diffs)
@@ -128,14 +128,21 @@ def extract_keyframes(video_path: str) -> [str]:
     success, frame = cap.read()
     idx = 0
     count = 0
+
+    ret = []
     while success:
         if idx in keyframe_id_set:
             count += 1
             name = "keyframe_" + str(count) + ".jpg"
-            cv2.imwrite(dir + name, frame)
+            ret.append(name)
+            cv2.imwrite(save_dir + name, frame)
             keyframe_id_set.remove(idx)
         idx = idx + 1
         success, frame = cap.read()
     cap.release()
 
-    return "keyframes/" + video_name
+    return ret
+
+
+if __name__ == '__main__':
+    print(extract_keyframes('output/1/1.mp4', 'output/'))
