cabf93d1d9b7ccbd0c55df08d746d919824edc4a
Hoshea <fgksgf@gmail.com>
2021-03-17 11:09:05 +0800

    增加超时处理

--- video_analysis/api.py
+++ video_analysis/api.py
@@ -1,10 +1,16 @@
 import logging
 
+import cv2
+from func_timeout import func_timeout, FunctionTimedOut
+
 import video_summary as vsumm
 import key_frame_extractor as keyframe_extractor
 
 logger = logging.getLogger('main.api')
 logger.setLevel(level=logging.INFO)
+
+# 处理时间不应超过视频时间（分钟）的倍数
+TIME_OUT_RATIO = 6
 
 
 def generate_abstract_from_video(file: str, save_dir: str) -> dict:
@@ -18,11 +24,15 @@ def generate_abstract_from_video(file: str, save_dir: str) -> dict:
         "VAbstract": [],  # 静态视频摘要文件名列表
         "Error": ""  # 错误信息
     }
+    video_min = get_video_duration(file)
     try:
         # 对视频进行摘要，得到浓缩版视频
         logger.info('Begin to do video summarize: %s, %s', file, save_dir)
-        compressed_video = video_summarize(file, save_dir)
+        compressed_video = func_timeout(video_min * TIME_OUT_RATIO * 60, video_summarize, args=(file, save_dir))
         logger.info('Done: video summarize: %s, %s', file, save_dir)
+    except FunctionTimedOut:
+        logger.error("summarize the video time out!")
+        ret['Error'] = '视频摘要超时'
     except Exception as e:
         logger.error(e, exc_info=True)
         ret['Error'] = 'failed to summarize the video: ' + str(e)
@@ -37,6 +47,19 @@ def generate_abstract_from_video(file: str, save_dir: str) -> dict:
             ret['Error'] = 'failed to extract key frames: ' + str(e)
 
     return ret
+
+
+def get_video_duration(file: str) -> int:
+    """
+    获取视频文件时长，以分钟为单位
+    :param file: 视频文件
+    :return: 视频时长，单位：分钟
+    """
+    video = cv2.VideoCapture(file)
+    fps = int(round(video.get(cv2.CAP_PROP_FPS)))  # 帧率
+    frame_counter = int(video.get(cv2.CAP_PROP_FRAME_COUNT))  # 总帧数
+    video.release()
+    return round(frame_counter / fps / 60)
 
 
 def extract_key_frame(file: str, save_dir: str) -> [str]:
