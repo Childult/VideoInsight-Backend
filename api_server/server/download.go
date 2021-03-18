6f613812ea272886576fd848d58ccc578c13d6e1
MoGuGuai-hzr <66289672+MoGuGuai-hzr@users.noreply.github.com>
2021-03-18 08:52:45 +0800

    修复Bug, 编译文件 (#29)

--- api_server/server/download.go
+++ api_server/server/download.go
@@ -87,7 +87,7 @@ func downloadHandle(job *job.Job, result []string) {
 
 	// 下载成功, 更新状态
 	logger.Debug.Printf("[下载视频回调] 视频下载成功: %+v.\n", pythonReturn)
-	r.VideoPath = result[0]
+	r.VideoPath = pythonReturn
 	r.SetStatus(util.ResourceExtracting)
 	job.SetStatus(util.JobExtractAudio)
 	go JobSchedule(job)
@@ -130,7 +130,7 @@ func extractHandle(job *job.Job, result []string) {
 	}
 
 	// 是否成功提取音频
-	if len(result) == 1 {
+	if len(result) == 0 {
 		logger.Error.Println("[提取音频回调] 提取失败.")
 		r.SetStatus(util.ResourceErrExtractFailed)
 		job.SetStatus(util.JobErrExtractFailed)
@@ -141,7 +141,7 @@ func extractHandle(job *job.Job, result []string) {
 
 	// 音频提取成功, 更新状态
 	logger.Debug.Printf("[提取音频回调] 音频提取成功: %+v.\n", pythonReturn)
-	r.AudioPath = result[0]
+	r.AudioPath = pythonReturn
 	r.SetStatus(util.ResourceCompleted)
 	job.SetStatus(util.JobExtractAudioDone)
 	go JobSchedule(job)
