132ea5b3e69e7a9a1c95666c300bb8f2a51b640e
Childult <35749727+Childult@users.noreply.github.com>
2021-04-07 10:49:46 +0800

    设置下载视频超时时间；增加获取视频标题函数 (#42)
    * 设置下载视频超时时间；获取视频后转换为mp4
    
    * base64改为url safe
    
    * 增加获取视频标题函数；超时时间改为函数参数

--- video_getter/api.py
+++ video_getter/api.py
@@ -4,23 +4,43 @@ import you_get
 import json
 import time
 import base64
+import signal, functools
 
 
-def download_video(url: str, path: str) -> str:
+def get_video_title(url: str) -> str:
+    """
+    获取视频标题
+    :param url: 视频URL
+    :return: 若成功返回视频标题，否则返回错误信息
+    """
+    try:
+        cmd = 'you-get ' + url + ' --json'
+        r = os.popen(cmd)
+        text = r.buffer.read().decode(encoding='utf8')
+        parsed_text = json.loads(text)
+        title = parsed_text['title']
+        res = title
+    except Exception as e:
+        res = e
+    return res
+
+
+def download_video(url: str, path: str, timeout: int=7200) -> str:
     """
     下载视频
     :param url: 视频URL
     :param path: 视频存储路径
+    :param timeout: 下载超时时间
     :return: 若成功返回文件名，否则返回错误信息
     """
-    cmd = 'you-get -o ' + path + ' ' + url + ' --json'
+    cmd = 'you-get ' + url + ' --json'
     r = os.popen(cmd)
     text = r.buffer.read().decode(encoding='utf8')
     parsed_text = json.loads(text)
-
+    
     # get title
     ts = time.time()
-    title = base64.b64encode((str(ts) + url).encode('utf8')).decode('utf-8')
+    title = base64.urlsafe_b64encode((str(ts) + url).encode('utf8')).decode('utf-8')
 
     # get accessible qualities (480p default), format and container
     qualities = ['480', '720', '360', '240', '1080']
@@ -35,13 +55,29 @@ def download_video(url: str, path: str) -> str:
     container = parsed_text['streams'][fmt]['container']
 
     sys.argv = ['you-get', '-o', path, '-O', title, url, '--format=%s' % fmt, '--debug']
+    file = title + '.' + container
+
+    def handler(signum, frame):
+        raise TimeoutError()
+    
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(timeout)
+
     try:
         you_get.main()
     except Exception as e:
-        return "Error: {0}".format(str(e))
-
-    return title + '.' + container
+        return "Error: {0}".format(e)
+    finally:
+        signal.alarm(0)
+        signal.signal(signal.SIGALRM, signal.SIG_DFL)
+        if not container == 'mp4':
+            os.system('ffmpeg -i ' + path + '/' + file + ' -vf scale=480:-1' + ' -vcodec h264 ' + title + '.mp4')
+            file = title + '.mp4'
+    
+    return file
 
 
 if __name__ == '__main__':
-    download_video('<test url>', '.')
+    test_url = '<test url>'
+    download_video(test_url, '.')
+    print(get_video_title(test_url))
