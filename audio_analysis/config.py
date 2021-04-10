1105fc2bdb2422fd2863e0e9883bfdc4c4ec58f0
Hoshea <fgksgf@gmail.com>
2021-04-10 11:14:32 +0800

    更新科大讯飞

--- audio_analysis/config.py
+++ audio_analysis/config.py
@@ -1,4 +1,4 @@
 class Config:
     # 科大讯飞
-    app_id = 'a385280f'
-    secrete_key = '0e4ef1b3548d449b206b97190e5c3e13'
+    app_id = 'f6a6d234'
+    secrete_key = 'fbe5dff8d8b840f3337799c52d75402f'
